68e4e4932b05b759edb9dd422c888d106403ac37
jomaytik <jomaytik@gmail.com>
2025-12-08 15:42:19 +0100

    feat: display course metadata with icons in formations carousel

--- classes/external/get_smartch_my_formations.php
+++ classes/external/get_smartch_my_formations.php
@@ -15,7 +15,7 @@
 // along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
 
 /**
- * Get course stats service
+ * Get smartch my formations - returns ALL published courses with metadata
  *
  * @package   theme_remui
  * @copyright (c) 2023 WisdmLabs (https://wisdmlabs.com/) <support@wisdmlabs.com>
@@ -32,12 +32,10 @@ use core_course_list_element;
 
 require_once($CFG->dirroot . '/course/lib.php');
 require_once($CFG->libdir . '/completionlib.php');
-// require_once($CFG->dirroot . '/course/lib.php');
-// require_once('./smartch_functions.php');
 
 /**
- * Get course stats trait
- * @copyright (c) 2022 WisdmLabs (https://wisdmlabs.com/) <support@wisdmlabs.com>
+ * Get smartch my formations trait
+ * @copyright (c) 2023 WisdmLabs (https://wisdmlabs.com/) <support@wisdmlabs.com>
  * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 trait get_smartch_my_formations
@@ -53,26 +51,101 @@ trait get_smartch_my_formations
         );
     }
 
+    /**
+     * ✅ FONCTION HELPER : Convertit la valeur numérique en texte
+     */
+    public static function get_select_option_text($fieldid, $value) {
+        global $DB;
+        
+        // Récupère la configuration du champ
+        $field = $DB->get_record('customfield_field', array('id' => $fieldid));
+        if (!$field) {
+            return '';
+        }
+        
+        // Décode le JSON configdata
+        $config = json_decode($field->configdata, true);
+        if (!isset($config['options'])) {
+            return '';
+        }
+        
+        // Split les options (séparées par \r\n)
+        $options = preg_split('/\r\n|\r|\n/', $config['options']);
+        
+        // La valeur 1 = première option, 2 = deuxième option, etc.
+        $index = intval($value) - 1;
+        
+        if (isset($options[$index])) {
+            return trim($options[$index]);
+        }
+        
+        return '';
+    }
 
+    /**
+     * Get all published courses with metadata (like UAT get_smartch_my_courses)
+     * @return string JSON encoded array of courses
+     */
     public static function get_smartch_my_formations()
     {
         global $DB, $CFG;
+
         $context = \context_system::instance();
         self::validate_context($context);
 
-        // Récupère TOUS les cours visibles (sauf le site)
-        $courses = $DB->get_records_sql('SELECT * FROM mdl_course WHERE visible = 1 AND format != "site" ORDER BY fullname ASC');
+        // ✅ Récupère TOUS les cours visibles (sauf le site)
+        $courses = $DB->get_records_sql(
+            'SELECT * FROM {course} WHERE visible = 1 AND format != ? ORDER BY fullname ASC',
+            array('site')
+        );
 
         $parcours = array();
+
         foreach ($courses as $course) {
+            $el = array();
             $el['fullname'] = $course->fullname;
             $el['id'] = $course->id;
             $el['url'] = $CFG->wwwroot . "/course/view.php?id=" . $course->id;
 
-            // Image du cours
-            $course2 = new \core_course_list_element($course);
-            $el['img'] = $CFG->wwwroot . '/theme/remui/pix/background.jpg'; // Image par défaut
+            // ✅ CORRECTION : Récupère les custom fields avec leur valeur TEXTE
+            $customfields_data = $DB->get_records_sql(
+                'SELECT cd.*, cf.shortname, cf.configdata, cf.id as fieldid
+                 FROM {customfield_data} cd
+                 JOIN {customfield_field} cf ON cf.id = cd.fieldid
+                 WHERE cd.instanceid = ?',
+                array($course->id)
+            );
 
+            $el['type'] = '';
+            $el['duration'] = '';
+
+            foreach ($customfields_data as $data) {
+                if ($data->shortname == 'type') {
+                    // ✅ Convertit "2" en "100% e-learning"
+                    $el['type'] = self::get_select_option_text($data->fieldid, $data->value);
+                } elseif ($data->shortname == 'duration') {
+                    $el['duration'] = $data->value;
+                }
+            }
+
+            // ✅ Détection automatique de la modalité (e-learning/hybride)
+            if ($el['type'] == '100% e-learning') {
+                $el['elearning'] = '100% e-learning';
+            } elseif ($el['type'] == 'Hybride') {
+                $el['hybride'] = 'Hybride';
+            }
+
+            // ✅ Catégorie
+            if ($course->category > 0) {
+                $category = $DB->get_record('course_categories', array('id' => $course->category));
+                if ($category) {
+                    $el['category'] = $category->name;
+                }
+            }
+
+            // ✅ Image du cours (comme UAT)
+            $imgcourse = "";
+            $course2 = new \core_course_list_element($course);
             foreach ($course2->get_course_overviewfiles() as $file) {
                 if ($file->is_valid_image()) {
                     $imagepath = '/' . $file->get_contextid() .
@@ -85,24 +158,27 @@ trait get_smartch_my_formations
                         $imagepath,
                         false
                     );
-                    $el['img'] = $imageurl;
+                    $imgcourse = $imageurl;
                     break;
                 }
             }
+            if ($imgcourse == "") {
+                $imgcourse = $CFG->wwwroot . '/theme/remui/pix/background.jpg';
+            }
+            $el['img'] = $imgcourse;
 
             array_push($parcours, $el);
         }
 
-        error_log("FORMATIONS COUNT: " . count($parcours));
         return json_encode($parcours);
     }
 
     /**
-     * Describes the get_smartch_my_courses return value
+     * Describes the get_smartch_my_formations return value
      * @return external_value
      */
     public static function get_smartch_my_formations_returns()
     {
-        return new external_value(PARAM_RAW, 'Courses of a user in JSON Format');
+        return new external_value(PARAM_RAW, 'Courses in JSON Format');
     }
 }
